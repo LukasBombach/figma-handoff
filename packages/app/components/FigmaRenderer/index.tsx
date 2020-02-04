462cf938fff46455088f72d12c9ee4d3f1d20274
Lukas Bombach <lukas.bombach@googlemail.com>
2020-02-04 14:31:13 +0100

    moved konva stage to canvas element

--- packages/app/components/FigmaRenderer/index.tsx
+++ packages/app/components/FigmaRenderer/index.tsx
@@ -1,61 +1,9 @@
-import { useState, useEffect, useRef, FC } from "react";
-import Konva from "konva";
-import { Stage, Layer } from "react-konva";
+import { FC } from "react";
 import { DOCUMENT } from "../../types/figma";
 import Document from "./nodes/Document";
 
 const FigmaRenderer: FC<{ document: DOCUMENT }> = ({ document }) => {
-  const [scale, setScale] = useState(1);
-  const [[x, y], setPosition] = useState([0, 0]);
-  const [[width, height], setSize] = useState([0, 0]);
-  const container = useRef<HTMLDivElement>(null);
-  const scaleBy = 1.1;
-
-  // todo create hook, ResizeObserver, unmount
-  useEffect(() => {
-    const width = container.current.offsetWidth;
-    const height = container.current.offsetHeight;
-    setSize([width, height]);
-  }, []);
-
-  // todo create hook
-  const handleWheel = ({ evt, target }: Konva.KonvaEventObject<WheelEvent>) => {
-    const stage = target.getStage();
-    const mouseX = stage.getPointerPosition().x / scale - stage.x() / scale;
-    const mouseY = stage.getPointerPosition().y / scale - stage.y() / scale;
-    const newScale = evt.deltaY > 0 ? scale * scaleBy : scale / scaleBy;
-    const newX = -(mouseX - stage.getPointerPosition().x / newScale) * newScale;
-    const newY = -(mouseY - stage.getPointerPosition().y / newScale) * newScale;
-    setScale(newScale);
-    setPosition([newX, newY]);
-    evt.preventDefault();
-  };
-
-  return (
-    <div className="container" ref={container}>
-      <Stage
-        width={width}
-        height={height}
-        style={{ border: "1px solid black" }}
-        draggable={true}
-        onWheel={handleWheel}
-        scaleX={scale}
-        scaleY={scale}
-        x={x}
-        y={y}
-      >
-        <Layer>
-          <Document document={document} />
-        </Layer>
-      </Stage>
-      <style jsx>{`
-        .container {
-          height: 100%;
-          width: 100%;
-        }
-      `}</style>
-    </div>
-  );
+  return <Document document={document} />;
 };
 
 export default FigmaRenderer;
--- packages/app/components/FigmaRenderer/nodes/Canvas.tsx
+++ packages/app/components/FigmaRenderer/nodes/Canvas.tsx
@@ -1,18 +1,62 @@
-import { FC } from "react";
-import { Rect } from "react-konva";
+import { useState, useEffect, useRef, FC } from "react";
+import Konva from "konva";
+import { Stage, Layer } from "react-konva";
 import { CANVAS } from "../../../types/figma";
-import Node from "./Node";
 import rgbaFromColor from "../rgba/fromColor";
+import Node from "./Node";
 
 const Canvas: FC<{ node: CANVAS }> = ({ node: canvas }) => {
-  const bgColor = rgbaFromColor(canvas.backgroundColor);
+  const [scale, setScale] = useState(1);
+  const [[x, y], setPosition] = useState([40, 40]);
+  const [[width, height], setSize] = useState([0, 0]);
+  const container = useRef<HTMLDivElement>(null);
   const visibleChildren = canvas.children.filter(c => c.visible !== false);
   const nodes = visibleChildren.map(c => <Node key={c.id} node={c} />);
+  const background = rgbaFromColor(canvas.backgroundColor);
+  const scaleBy = 1.1;
+
+  // todo create hook, ResizeObserver, unmount
+  useEffect(() => {
+    const width = container.current.offsetWidth;
+    const height = container.current.offsetHeight;
+    setSize([width, height]);
+  }, []);
+
+  // todo create hook
+  const handleWheel = ({ evt, target }: Konva.KonvaEventObject<WheelEvent>) => {
+    const stage = target.getStage();
+    const mouseX = stage.getPointerPosition().x / scale - stage.x() / scale;
+    const mouseY = stage.getPointerPosition().y / scale - stage.y() / scale;
+    const newScale = evt.deltaY > 0 ? scale * scaleBy : scale / scaleBy;
+    const newX = -(mouseX - stage.getPointerPosition().x / newScale) * newScale;
+    const newY = -(mouseY - stage.getPointerPosition().y / newScale) * newScale;
+    setScale(newScale);
+    setPosition([newX, newY]);
+    evt.preventDefault();
+  };
+
   return (
-    <>
-      <Rect x={0} y={0} width={800} height={600} fill={bgColor}></Rect>
-      {nodes}
-    </>
+    <div className="container" ref={container}>
+      <Stage
+        width={width}
+        height={height}
+        style={{ background }}
+        draggable={true}
+        onWheel={handleWheel}
+        scaleX={scale}
+        scaleY={scale}
+        x={x}
+        y={y}
+      >
+        <Layer>{nodes}</Layer>
+      </Stage>
+      <style jsx>{`
+        .container {
+          height: 100%;
+          width: 100%;
+        }
+      `}</style>
+    </div>
   );
 };
 
